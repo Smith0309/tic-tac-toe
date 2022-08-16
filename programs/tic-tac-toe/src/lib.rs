2dc47f35880bbc211f6ca88bc6e082ffd6da61ca
smile930307 <ImamEugenaU2K@gmail.com>
2022-08-17 06:13:51 +0900

    add unit test

--- programs/tic-tac-toe/src/lib.rs
+++ programs/tic-tac-toe/src/lib.rs
@@ -11,6 +11,26 @@ pub mod tic_tac_toe {
     pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> Result<()> {
         ctx.accounts.game.start([ctx.accounts.player_one.key(), player_two])
     }
+
+    pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
+        let game = &mut ctx.accounts.game;
+    
+        require_keys_eq!(
+            game.current_player(),
+            ctx.accounts.player.key(),
+            TicTacToeError::NotPlayersTurn
+        );
+    
+        game.play(&tile)
+    }
+
+}
+
+#[derive(Accounts)]
+pub struct Play<'info> {
+    #[account(mut)]
+    pub game: Account<'info, Game>,
+    pub player: Signer<'info>,
 }
 
 #[derive(Accounts)]
